bd0c7d85f5bc29f24d80785b4a2b6ffdc1721a98
nec2dev <nec2multivackergithub@outlook.com>
2021-04-05 20:56:51 -0300

    changes

--- tp1.1.c
+++ tp1.1.c
@@ -2,11 +2,16 @@
 variables indicadas a continuación, si serealizan las operaciones 
 que se indican, sabiendo que se han definido las variables usadas 
 de lasiguiente forma, e indicándose sus valores iniciales:*/
+#include <stdio.h>
+int main() {
 
-int a=3,b=2,c=1, d,e;
-float m=2.5, n=5.4, r,s;
-d= m;
-d=n;
+    int a=3,b=2,c=1, d,e;
+    float m=2.5, n=5.4, r,s;
+
+    d=m;
+    printf('d= ' + d + '\n');
+
+/*d=n;
 e=3.7;
 d=a;
 d=a+b;
@@ -20,4 +25,16 @@ e=c/b;
 r=a+b;
 r=a/b;
 r=a/2;
-r=a/2.0;q)s=m+n;r)s=3+4-1;s)s=3.0+4.0-1;t)a=m;u)a=m/2;v)a=m/2.0;w)a=3.0+4.0-1;x)r=a+1;y)r=a+1.0;z)a++;
+r=a/2.0;
+s=m+n;
+s=3+4-1;
+s=3.0+4.0-1;
+a=m;
+a=m/2;
+a=m/2.0;
+a=3.0+4.0-1;
+r=a+1;
+r=a+1.0;
+a++;*/
+
+};
