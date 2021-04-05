379bdba8a7edd0cfcc48ebfe3ad45f11f0ddefac
nec2dev <nec2multivackergithub@outlook.com>
2021-04-05 20:47:54 -0300

    tp1.1

---
+++ tp1.1.c
@@ -0,0 +1,23 @@
+/*1) Indicar qué es lo que se almacena en cada una de las 
+variables indicadas a continuación, si serealizan las operaciones 
+que se indican, sabiendo que se han definido las variables usadas 
+de lasiguiente forma, e indicándose sus valores iniciales:*/
+
+int a=3,b=2,c=1, d,e;
+float m=2.5, n=5.4, r,s;
+d= m;
+d=n;
+e=3.7;
+d=a;
+d=a+b;
+d=a*b;
+d=a/b;
+d=b/4;
+d=a%b;
+d=a%2;
+e=b/c;
+e=c/b;
+r=a+b;
+r=a/b;
+r=a/2;
+r=a/2.0;q)s=m+n;r)s=3+4-1;s)s=3.0+4.0-1;t)a=m;u)a=m/2;v)a=m/2.0;w)a=3.0+4.0-1;x)r=a+1;y)r=a+1.0;z)a++;
