d389eeac173c4c02526bbfe3e032c34eea019610
nec2dev <nec2multivackergithub@outlook.com>
2021-04-05 22:42:35 -0300

    a=!b

---
+++ tp1_3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+
+    int a=2, b=3, c=0, d =0, e=1;;
+    
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("b) = %s", ( (4<5) && (2>2))? "true" : "false");
+    printf("\n");
+
+    printf("c) = %s", a=!b? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    printf("a) = %s", 5>3? "true" : "false");
+    printf("\n");
+
+    return(0);
+    };
