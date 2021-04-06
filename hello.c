b15a99f31dde832e33adf88219d04ac7feaeb161
nec2dev <nec2multivackergithub@outlook.com>
2021-04-05 21:34:30 -0300

    changes

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
-/*#include <stdio.h>
+#include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!\n\n\n\n");
    printf("Bye, World!\n\n\n\n");
    return 0;
-};*/
+};
