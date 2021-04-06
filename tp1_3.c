e7e0a5711f300195cde48fa479fa2a08d1257c62
nec2dev <nec2multivackergithub@outlook.com>
2021-04-05 22:45:32 -0300

    end

--- tp1_3.c
+++ tp1_3.c
@@ -4,34 +4,34 @@ int main(){
 
     int a=2, b=3, c=0, d =0, e=1;;
     
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("a) = %s", 5>3 ? "true" : "false");
     printf("\n");
 
-    printf("b) = %s", ( (4<5) && (2>2))? "true" : "false");
+    printf("b) = %s", ( (4<5) && (2>2)) ? "true" : "false");
     printf("\n");
 
-    printf("c) = %s", a=!b? "true" : "false");
+    printf("c) = %s", !b ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("d) = %s", ((a%2 ==0) || (a-b <10)) ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("e) = %s", (d && e) ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("f) = %s", (d || e) ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("g) = %s", !(a && b) ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("h) = %s", (! a) && (!b) ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("i) = %s", (a && (!a)) ? "true" : "false");
     printf("\n");
 
-    printf("a) = %s", 5>3? "true" : "false");
+    printf("j) = %s", (((a<=(b*3.2)) &&(b%2!=0))|| (1)) ? "true" : "false");
     printf("\n");
 
     return(0);
