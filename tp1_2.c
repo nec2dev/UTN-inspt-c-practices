e37d16c28b18491884d6a3ab1907129c3e6cc7b9
nec2dev <nec2multivackergithub@outlook.com>
2021-04-05 22:24:19 -0300

    tabulación

--- tp1_2.c
+++ tp1_2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    
-    printf( " Me gusta C" );
-    printf (“ Fin del mensaje\n”);
-    printf (“Adios”);
+
+    printf("Me gusta C\n\n" );
+    printf ("Fin del mensaje\n");
+    printf ("\tAdios");
     
     return(0);
     }
