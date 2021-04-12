3f48b3509a455e513b0c5a542fe74b44656abc66
nec2dev <nec2multivackergithub@outlook.com>
2021-04-12 17:05:33 -0300

    guia 1.3

---
+++ guia1_3.c
@@ -0,0 +1,14 @@
+// 3.Colocalos signos de puntuación donde correspondan.
+#include <stdio.h>
+int main(){
+
+    int pies, pulgadas;
+
+    pies = 5;
+    pulgadas = pies*12;
+
+    printf("%d", pulgadas);
+
+    return(0);
+
+    };
