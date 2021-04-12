be6e58d1220e3e37777c622eb579cb1ec3f2611f
nec2dev <nec2multivackergithub@outlook.com>
2021-04-12 15:04:16 -0300

    horas minutos segundos

--- problem002.c
+++ problem002.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 int main() {
 
+    int hora1, minuto1, segundo1, hora2, minuto2, segundo2;
+    double tiempo1, tiempo2;
+   
+   printf("Por favor, Ingrese la o las horas del primer tiempo\n");
+   scanf("%d", &hora1);
+   printf("Por favor, Ingrese el o los minutos del primer tiempo\n");
+   scanf("%d", &minuto1);
+   printf("Por favor, Ingrese el o los segundos del primer tiempo\n");
+   scanf("%d", &segundo1);
+
+   printf("Por favor, Ingrese la o las horas del segundo tiempo\n");
+   scanf("%d", &hora2);
+   printf("Por favor, Ingrese el o los minutos del segundo tiempo\n");
+   scanf("%d", &minuto2);
+   printf("Por favor, Ingrese el o los segundos del segundo tiempo\n");
+   scanf("%d", &segundo2);
+
+    int segundoauxiliar = segundo1 + segundo2;
+    int segundoresto = segundoauxiliar % 60;                                             
+    int minutoauxiliar = minuto1 + minuto2 + segundoauxiliar/60;
+    int minutoresto = minutoauxiliar % 60;
+    int horasresto = tiempo1 + tiempo2 + minutoauxiliar/60;​
     
+    printf(“Resultado de la suma de los tiempos ingresados \n”,
+    printf(“ %d  horas %d minutos  %d segundos”, horasresto, minutoresto, segundoresto);​
    
    return 0;
+
 };
