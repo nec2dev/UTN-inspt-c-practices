e7eb87a70a18a11037ac0c950feff60b053bb72b
nec2dev <nec2multivackergithub@outlook.com>
2021-04-12 15:53:07 -0300

    tp1.8

---
+++ tp1_8.c
@@ -0,0 +1,14 @@
+/*8) Considere este esquema de programa y
+a) Coloque el/los headers adecuados
+b) Complete el programa para que se emita el perímetro y 
+la superficie del cuadrado cuyo lado se haingresado.
+c) Investigue qué sucede si se coloca un especificador equivocado 
+en el ingreso de datos.
+d)Observe qué ocurre si se omite el & en la sentencia de ingreso 
+de datos*/
+#include <stdio.h>
+int main(){
+    float lado, perimetro, superficie;printf (“Ingrese la medida del lado de un cuadrado:”);
+    scanf (“%f”, &lado);// completar aquí
+    return 0;
+    };
