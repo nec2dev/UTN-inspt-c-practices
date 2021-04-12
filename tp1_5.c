acb1e8206dd696acd7a373dd86e8cbdcfc101d7f
nec2dev <nec2multivackergithub@outlook.com>
2021-04-12 15:50:01 -0300

    tp1.5

---
+++ tp1_5.c
@@ -0,0 +1,14 @@
+/*5) Indicar qué queda almacenado en la variable con la que se 
+ingresan datos en cada caso, si se handefinido las variables de 
+la siguiente forma:int a;float b;char c;
+a)scanf (“%d”, &a);
+b)scanf (“%f”, &a);
+c)scanf (“%c”, &a);
+d)scanf (“%d”, &b);
+e)scanf (“%f”, &b);
+f)scanf (“%c”, &b);
+g)scanf (“%d”, &c);
+h)scanf (“%f”, &c);
+i)scanf (“%f”, &c);
+j)A partir de las situaciones anteriores, sintetice el uso de estos 
+especificadores para laentrada %d, %o, %b,  %x, %c, %f*/
---
+++ tp1_6.c
@@ -0,0 +1,2 @@
+/*6) Indique funciones del preprocesador, compilador y enlazador.
+Especifique salida de cada uno.*/
