f25daa44340ec26aec97e75753b9409e6de91e23
nec2dev <nec2multivackergithub@outlook.com>
2021-04-19 15:40:42 -0300

    more changes

--- Problema003.c
+++ Problema003.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 int main() {
 
-    float purchaseAmount, wayToPayA, wayToPayB, wayToPayC;
+    float purchaseAmount;
     char wayToPay;
    
    printf("Por favor, Ingrese el valor de la compra\n");
    scanf("%f", &purchaseAmount);
    printf("Ahora, ingrese la forma de pago\n");
    fflush(stdin);
-   scanf("%C", &wayToPay);
+   scanf("%c", &wayToPay);
    
-    float cashPaymentDiscount (float purchaseAmount){
-        wayToPayA = purchaseAmount-(purchaseAmount*0.15);
+    float cashPaymentDiscount(float purchaseAmount){
+        float wayToPayA = purchaseAmount-(purchaseAmount*0.15);
         return wayToPayA;
      };
 
-    float cardPaymentOneFee (float purchaseAmount){
-        wayToPayB = purchaseAmount+(purchaseAmount*0.1);
+    float cardPaymentOneFee(float purchaseAmount){
+        float wayToPayB = purchaseAmount+(purchaseAmount*0.1);
         return wayToPayB;
      };
 
-    float cardPaymentThreeFees (float purchaseAmount){
-        wayToPayC = purchaseAmount+(purchaseAmount*0.2);
+    float cardPaymentThreeFees(float purchaseAmount){
+        float wayToPayC = purchaseAmount+(purchaseAmount*0.2);
         return wayToPayC;
+     };
+
+     float cardPaymentTwleveFees(float purchaseAmount){
+        float wayToPayD = purchaseAmount;
+        return wayToPayD;
      };
 
    if(wayToPay =='a'){
@@ -38,11 +43,15 @@ int main() {
        printf("Debe pagar un total de: $ %.2f\n",cardPaymentThreeFees(purchaseAmount));
        printf("Cada cuota es de: $ %.2f\n", cardPaymentThreeFees(purchaseAmount)/3);
    }
-   else {
+   else if (wayToPay =='d'){
        printf("Pago con tarjeta en doce cuotas\n");
-       printf("Debe pagar un total de: $ %.2f\n", purchaseAmount);
+       printf("Debe pagar un total de: $ %.2f\n", cardPaymentTwleveFees(purchaseAmount));
        printf("Cada cuota es de: $ %.2f\n", purchaseAmount/12);
    }
+   else{
+       printf("Ha ingresado un valor no válido\n");
+   };
 
    return 0;
+
 };
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,9 +1,1 @@
-if((z=3*x+6)==(z+=6)){
-//        if(z=3*x+6)==(z*=3)){
-//            printf("Su expresión equivalente es: z += 6*x\n");
-//        }
-//        }else{
-//            printf("Su expresión equivalente es: y /= 10 + y/x\n");
-//            }else{
-//                printf("La expresión equivalente no es posible\n");
-//    };
+purchaseAmount
