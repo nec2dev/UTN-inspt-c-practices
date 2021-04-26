bf29425ef3c7af42132cb80470e4f5bb6d4c0e35
nec2dev <nec2multivackergithub@outlook.com>
2021-04-26 16:14:58 -0300

    to upper

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,4 @@
-if (num > mayor)
-            //   mayor=num;
+if(desicion1=='N')
+    //     printf("Buenas, el circuito está apagado.\n");  
+    // else
+    //     printf("No ha ingresado un carácter válido.\n");
--- tp1_1.c
+++ tp1_1.c
@@ -19,36 +19,40 @@ int main(){
     printf("switch003: %i\n", switch003); 
     printf("Desea encender algún interruptor?, Ingrese S o N\n");
     scanf("%c", &desicion1);
-    toupper(desicion1);
 
-    if(desicion1=='N' || desicion1=='S')
-        printf(""),
-    else
-        printf("No ha ingresado un carácter válido.\n")
-
-    if(desicion1=='N')
-        printf("Buenas, el circuito está apagado.\n");  
-        exit(-1);
-        if(desicion1=='S')
-            switch001 = 1;
-            printf("switch001: %i\n", switch001); 
-            printf("switch002: %i\n", switch002); 
-            printf("switch003: %i\n", switch003); 
-            printf("El circuito no está funcionando, por favor encienda un interruptor mas.");  
-            printf("Desea encender algún interruptor?, Ingrese S o N\n");
-            scanf("%c", &desicion2); 
-                if(desicion2=='N')
-                    printf("El circuito no está funcionando.");    
-                else(desicion2=='S');
-                switch002 = 1;
-                printf("switch001: %i\n", switch001); 
-                printf("switch002: %i\n", switch002); 
-                printf("switch003: %i\n", switch003); 
-                printf("El circuito ya está funcionando."); 
-
-    return 0;
-            
-    // else
-    //     ;  
+    if(desicion1=='S'){
+        switch001 = 1;
+        printf("switch001: %i\n", switch001); 
+        printf("switch002: %i\n", switch002); 
+        printf("switch003: %i\n", switch003); 
+        printf("El circuito todavía no está funcionando, por favor encienda un interruptor mas.\n");  
+        printf("Desea encender algún interruptor?, Ingrese S o N\n");
+        scanf("%c", &desicion2); 
     
+    if(desicion2=='S')
+        switch002 = 1;
+        printf("switch001: %i\n", switch001); 
+        printf("switch002: %i\n", switch002); 
+        printf("switch003: %i\n", switch003); 
+        printf("El circuito ya está funcionando."); 
+    
+    if(desicion1=='N'){
+        printf("switch001: %i\n", switch001); 
+        printf("switch002: %i\n", switch002); 
+        printf("switch003: %i\n", switch003); 
+        printf("El circuito quedó apagdo."); 
+    
+    if(desicion2=='N')
+        printf("switch001: %i\n", switch001); 
+        printf("switch002: %i\n", switch002); 
+        printf("switch003: %i\n", switch003); 
+        printf("El circuito quedó apagdo con un switch prendido."); 
+    }
+    else{
+        printf("El circuitoquedó apagado");
+        }
+        }
+        }
+        }
+
 };
