19790d327533335d5357b261fc7a6b4c182e407c
nec2dev <nec2multivackergithub@outlook.com>
2021-06-13 12:48:29 -0300

    2 50ok

---
+++ guia2_50.c
@@ -0,0 +1,5 @@
+/*50.Sea la siguiente lista de palabras: int, float, 
+upper, void, struct, root, fseek, else, double, const, 
+return, char, si, enum, for, while, long, unsigned. 
+¿cuántas de ellas podrían utilizarse en un programa en C 
+sin ocasionar conflictos?*/
