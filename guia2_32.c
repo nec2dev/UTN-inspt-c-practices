a6920dec08d10552eeb689b8968eb56a7e21a5f4
nec2dev <nec2multivackergithub@outlook.com>
2021-06-13 09:26:02 -0300

    guia2 32ok

---
+++ guia2_32.c
@@ -0,0 +1,21 @@
+/*32.En un almacén se rebaja 20% del precio al cliente 
+si el valor a pagarse es mayor a $200. Dado un valor, 
+muestre lo que debe pagar el cliente.*/
+
+#include <stdio.h>
+
+int main (){
+    
+     float preciocompra, preciofinal;
+
+     printf("Ingrese el valor de la compra: \n");
+     scanf("%f", &preciocompra);
+     
+     if(preciocompra>=200)
+     preciofinal=preciocompra-(preciocompra*0.2);
+     printf("El valor de la compra final es $ %c: \n", preciofinal);
+     else
+     printf("El valor de la compra final es $ %c: \n", preciocompra);
+
+     return 0;
+};
