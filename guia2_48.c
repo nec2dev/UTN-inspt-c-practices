0d759c49dc7dc71ba2d14d51ac4712a516287a4d
nec2dev <nec2multivackergithub@outlook.com>
2021-06-13 12:40:09 -0300

    2 48ok

---
+++ guia2_48.c
@@ -0,0 +1,15 @@
+/*48.¿Qué definición es la correcta en lenguaje C?*/
+
+#include <stdio.h>
+
+int main(){
+     
+    //#define n=25;
+    //#define n 25
+    int n=25;
+    //int n 25
+
+    printf("%d", n);
+
+    return(0);
+}
