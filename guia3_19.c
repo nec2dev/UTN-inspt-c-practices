1f031d51e943255b7a16984fe64e8590f1e1505d
nec2dev <nec2multivackergithub@outlook.com>
2021-06-14 08:25:45 -0300

    3 19ok

---
+++ guia3_19.c
@@ -0,0 +1,25 @@
+/*Se lee un número entero N mayor o igual que 0 y, 
+la computadora muestra el factorial del mismo.*/
+
+#include <stdio.h>
+
+int main (){
+    int n, i; 
+    
+    //pedimos un numero positivo
+    printf("ingrese un numero natural:\n");
+    scanf("%d",&n);
+
+    while(n<=0){
+       printf("el numero debe ser igual o mayor a cero:\n");
+       scanf("%d",&n);
+    }    
+    
+    //imprimir los factores
+    for(i=n;i>=0;i--)
+       if(i%2==0)
+       printf("%d\n",i);   
+       getchar();
+
+    return 0;
+}
---
+++ guia3_20.c
@@ -0,0 +1,5 @@
+/*Se lee un número real L perteneciente al intervalo 
+[1,10], y la computadora informa la cantidad de términos 
+de la serie armónica necesarios para satisfacer la 
+desigualdad: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n  >  L 
+*/
