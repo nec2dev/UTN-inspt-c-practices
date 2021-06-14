c738a8b01b1bf92fd75067fd52543b67d6a28e46
nec2dev <nec2multivackergithub@outlook.com>
2021-06-14 08:39:41 -0300

    3 20ok

--- guia3_20.c
+++ guia3_20.c
@@ -1,5 +1,30 @@
 /*Se lee un número real L perteneciente al intervalo 
 [1,10], y la computadora informa la cantidad de términos 
 de la serie armónica necesarios para satisfacer la 
-desigualdad: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n  >  L 
-*/
+desigualdad: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n  >  L */
+
+#include <stdio.h>
+
+int main (){
+
+    int cuenta=0;
+    float l; 
+    double suma=0;
+    
+    //pedimos un numero positivo entre 1 y 10
+    printf("ingrese un numero real entre 1 y 10:\n");
+    scanf("%f",&l);
+
+    while(!(l>=1&&l<=10)){
+       printf("el numero debe tener un valor entre 1 y 10:\n");
+       scanf("%f",&l);
+    }    
+    
+    while(suma<l){
+        cuenta++;
+        suma=suma+(1/(cuenta*1.0));
+    }
+        printf("\nValores:\n\nCuenta= %d \nSuma= %.18f",cuenta,suma);
+
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,18 +1,4 @@
-if(real>0){
-        //     cantpos++;
-        //     sumapos=+real;
-        // }
-        // prompos=sumapos/cantpos;
-        // printf("El promedio de los numero positivos ingresados es: %f\n", prompos);
-
-        // if(real<0){
-        //     cantneg++;
-        //     sumaneg=+real;
-        // }
-        // promneg=sumaneg/cantneg;
-        // printf("El promedio de los numero negativos ingresados es: %f\n", promneg);
-
-        // if(real==0){
-        //     cantceros++;
-        //     printf("La cantidad de ceros fue: %d\n", cantceros);
-        // }
+while(l>=1&&l<=10){
+    //    printf("el numero debe tener un valor entre 1 y 10:\n");
+    //    scanf("%f",&l);
+    // }
