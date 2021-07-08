3faa193a2fd8aca2bd6b74593ac4a1bf0a71ffdd
nec2dev <nec2multivackergithub@outlook.com>
2021-07-08 11:32:17 -0300

    3 27 to fix

---
+++ guia3_27.c
@@ -0,0 +1,22 @@
+/*27) Los antiguos griegos llamaban números perfectos a aquellos que son 
+iguales a la suma de sus divisores (exceptuando al mismo número),como por 
+ejemplo el 6=1+2+3.Escribir un programa que lea un número N y emita los 
+N primeros números perfectos.*/
+
+#include <stdio.h>
+
+int main (){
+
+    int num, sum=0,i;
+
+    printf("Ingrese un numero entero: \n");
+    scanf("%d", &num);
+
+    printf("%d= ", num);
+    for(i=1; i<num; i++){
+        if(num%i==0){
+        printf("%d + ", i);
+        }
+    }
+    return 0;
+}
