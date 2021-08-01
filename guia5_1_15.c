d657c881c7f1a9728ca072c914797fe1a46cf4d0
nec2dev <nec2multivackergithub@outlook.com>
2021-08-01 09:52:57 -0300

    Ej 5 1 15 wrong

--- guia5_1_15.c
+++ guia5_1_15.c
@@ -1,38 +1,31 @@
 /*15.Escribir una funcion void Carga_Pri (int x, int *pos) que recibe un 
 entero x y una dirección pos y carga en pos el número primo más cercano 
 a x, que no seas inferio a él. Ejemplo: 
-//si se tiene
-int h=9;
-int b;
-//y se invoca
-Carga_Pri (h; &b);
-//se carga en b el número primo 11, que es el primer primo mayor o igual 
-que 9.
-*/
+si se tiene int h=9; int b; y se invoca Carga_Pri (h; &b);
+se carga en b el número primo 11, que es el primer primo mayor que 9.*/
 
 #include <stdio.h>
 
 void Carga_Pri(int x, int *pos){
-    for(int i=1; i<=20; i++){
-        int primo = 1;
-        int contador = 2;
+    for(int i=1; i==x; i++){
+        int primo=1;
+        int contador=2;
         while (contador<=i/2 && primo){
             if(i%contador==0)
                 primo = 0;
                 contador++;
         }
         if(primo)
-            *pos=i;
+            pos=i;
     }
-    getch();
 }
 
 int main(){
-
     int x, *pos;
-    printf("Ingrese un valor para x");
+    printf("Ingrese un valor para x\n");
     scanf("%d", x);
-    Carga_Pri(x, &pos);
+    pos=0;
+    Carga_Pri(x, *pos);
     printf("%d", pos);
     return(0); 
 }
